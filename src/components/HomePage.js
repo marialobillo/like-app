8d0ebbe91cb36374307c052433fbecba3288d51b
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-03-15 10:29:12 +0100

    homepage

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+var React = require('react')
+
+class HomePage extends React.Component{
+	render(){
+		return (
+			<div>
+				<h1>Welcome to Cat and Dog Cuteness Fight Game!</h1>
+			</div>
+		);
+	}
+}
+
+
+module.exports = HomePage;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+
+ReactDOM.render(
+		<HomePage />,
+		document.getElementById('root')
+);
