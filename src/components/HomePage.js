e64615c1754c3bea3a8da2a9df63da65331a1343
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-03-17 15:53:19 +0100

    some style

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,14 +2,33 @@ var React = require('react')
 var PetComponent = require('./PetComponent');
 
 
-var style = {
-	textAlign : 'center',
-	fontSize: '2em',
-	color: 'rebeccapurple',
-	margin: '15px'
-};
-
 class HomePage extends React.Component{
+	constructor(props){
+		super(props);
+		this.catCompInstRef = null;
+		this.dogCompInstRef = null;
+		this.handleShowWinnerBtnClick = this.handleShowWinnerBtnClick.bind(this);
+		this.handleStartOverBtnClick = this.handleStartOverBtnClick.bind(this);
+	}
+	handleShowWinnerBtnClick(){
+		console.log(this.catCompInstRef);
+		console.log(this.dogCompInstRef);
+
+		var catLikesCount = this.catCompInstRef.state.likesCount;
+		var dogLikesCount = this.dogCompInstRef.state.likesCount;
+
+		if(catLikesCount > dogLikesCount){
+			console.log('Cat is the Winner');
+		} else if (catLikesCount < dogLikesCount){
+			console.log('Dog is the Winner');
+		} else {
+			console.log('Game is a TIE');
+		}
+
+	}
+	handleStartOverBtnClick(){
+
+	}
 	render(){
 		return (
 			<div className="container">
@@ -18,12 +37,24 @@ class HomePage extends React.Component{
 					<div className="row">
 								<PetComponent
 									petName="Cat"
+									ref={(compInst) => { this.catCompInstRef = compInst;}}
 									petImageUrl="https://i.pinimg.com/originals/05/83/69/05836996c7f72c287ad227a937b03dc7.jpg"
 								/>
 								<PetComponent
 									petName="Dog"
+									ref={(compInst) => { this.dogCompInstRef = compInst;}}
 									petImageUrl="https://thiswallpaper.com/cdn/hdwallpapers/649/beautiful%20cute%20dog%20high%20resolution%20wallpaper.jpg"
 								/>
+					</div>
+					<div className="row boton">
+						<button
+							className="btn btn-lg btn-warning"
+							onClick={this.handleShowWinnerBtnClick}
+							>Show Winner</button>
+						<button
+							className="btn btn-lg btn-info"
+							onClick={this.handleStartOverBtnClick}
+							>Start Over</button>
 					</div>
 			</div>
 		);
