da42b2f1f275900b9796697bcaeb97636ed5be46
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-03-17 18:18:21 +0100

    refactor petcomponent

--- src/components/PetComponent.js
+++ src/components/PetComponent.js
@@ -1,35 +1,30 @@
 var React = require('react');
 
-class PetComponent extends React.Component{
-	constructor(props){
-		super(props);
 
-	}
-
-	render(){
-		return (
-			<div className="comp">
-				<h3>{this.props.petName} Likes: {this.props.likesCount}</h3>
-				<img
-					src={this.props.petImageUrl}
-					alt="cute pet"
-					className="pet_image"
-				/>
-				<br />
-				<button
-					className="btn btn-warning btn-lg pet_btn"
-					onClick={this.props.onLikeBtnClick}
-					value={this.props.petName}
-					>Like</button>
-				<button
-					className="btn btn-secondary btn-lg pet_btn"
-					onClick={this.props.onDislikeBtnClick}
-					value={this.props.petName}
-					>Dislike</button>
-			</div>
-		);
-	}
+var PetComponent = function(props){
+	return (
+		<div className="comp">
+			<h3>{props.petName} Likes: {props.likesCount}</h3>
+			<img
+				src={props.petImageUrl}
+				alt="cute pet"
+				className="pet_image"
+			/>
+			<br />
+			<button
+				className="btn btn-warning btn-lg pet_btn"
+				onClick={props.onLikeBtnClick}
+				value={props.petName}
+				>Like</button>
+			<button
+				className="btn btn-secondary btn-lg pet_btn"
+				onClick={props.onDislikeBtnClick}
+				value={props.petName}
+				>Dislike</button>
+		</div>
+	);
 }
+
 
 
 module.exports = PetComponent;
